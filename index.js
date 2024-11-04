99634ce5b71e3ba00013ed5429eeec7d3adb0a0e
norton-kb <biinorton080@gmail.com>
2024-11-04 14:46:27 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        const message = `Thank you, ${name}, for the wonderful ${event} gift!`;
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
